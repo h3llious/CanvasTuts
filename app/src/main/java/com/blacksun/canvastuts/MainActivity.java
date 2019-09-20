d51d134597b35327cbea95ecb82916893cf189b3
Anh Nhat Bui <anhnhatbuiit@gmail.com>
2019-09-20 11:27:58 +0700

    Polish code

--- app/src/main/java/com/blacksun/canvastuts/MainActivity.java
+++ app/src/main/java/com/blacksun/canvastuts/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
 import android.view.FrameMetrics;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button mButton;
+    HandlerThread handlerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +24,36 @@ public class MainActivity extends AppCompatActivity {
 
         final FrameMetrics[] metrics = {null};
         Window window = getWindow();
-        Handler mHandler = new Handler();
+        handlerThread = new HandlerThread("Thread1");
+        handlerThread.start();
+        final Handler mHandler = new Handler(handlerThread.getLooper());
         window.addOnFrameMetricsAvailableListener(new Window.OnFrameMetricsAvailableListener() {
             @Override
-            public void onFrameMetricsAvailable(Window window, FrameMetrics frameMetrics, int i) {
-                metrics[0] = new FrameMetrics(frameMetrics);
+            public void onFrameMetricsAvailable(Window window, final FrameMetrics frameMetrics, int i) {
+//                metrics[0] = new FrameMetrics(frameMetrics);
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("Metrics", "===============================================");
+                        // Send this data to custom analytcis
+                        Log.d("Metrics", "ANIMATION_DURATION: " + frameMetrics.getMetric((FrameMetrics.ANIMATION_DURATION))/Math.pow(10,6));
+                        Log.d("Metrics", "COMMAND_ISSUE_DURATION: " + frameMetrics.getMetric(FrameMetrics.COMMAND_ISSUE_DURATION)/Math.pow(10,6));
+                        Log.d("Metrics", "DRAW_DURATION: " + frameMetrics.getMetric(FrameMetrics.DRAW_DURATION)/Math.pow(10,6));
+                        Log.d("Metrics", "FIRST_DRAW_FRAME: " + frameMetrics.getMetric(FrameMetrics.FIRST_DRAW_FRAME)/Math.pow(10,6));
+                        Log.d("Metrics", "INPUT_HANDLING_DURATION: " + frameMetrics.getMetric(FrameMetrics.INPUT_HANDLING_DURATION)/Math.pow(10,6));
+                        Log.d("Metrics", "LAYOUT_MEASURE_DURATION: " + frameMetrics.getMetric(FrameMetrics.LAYOUT_MEASURE_DURATION)/Math.pow(10,6));
+                        Log.d("Metrics", "SWAP_BUFFERS_DURATION: " + frameMetrics.getMetric(FrameMetrics.SWAP_BUFFERS_DURATION)/Math.pow(10,6));
+                        Log.d("Metrics", "SYNC_DURATION: " + frameMetrics.getMetric(FrameMetrics.SYNC_DURATION)/Math.pow(10,6));
+                        Log.d("Metrics", "TOTAL_DURATION: " + frameMetrics.getMetric(FrameMetrics.TOTAL_DURATION)/Math.pow(10,6));
+                        Log.d("Metrics", "UNKNOWN_DELAY_DURATION: " + frameMetrics.getMetric(FrameMetrics.UNKNOWN_DELAY_DURATION)/Math.pow(10,6));
+//                );
+                        Log.d("Metrics", "===============================================");
+                    }
+                });
             }
         },mHandler);
 
-        mHandler.postDelayed(new Runnable() {
+        /*mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 // Send this data to custom analytcis
@@ -46,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Metrics", "UNKNOWN_DELAY_DURATION: " + metrics[0].getMetric(FrameMetrics.UNKNOWN_DELAY_DURATION)/Math.pow(10,6));
 //                );
             }
-        }, 1000);
+        }, 1000);*/
 
         mButton = findViewById(R.id.btn_switch_to_5);
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -56,5 +79,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(handlerThread != null && handlerThread.isAlive()) {
+            handlerThread.quit();
+        }
+        super.onDestroy();
     }
 }
